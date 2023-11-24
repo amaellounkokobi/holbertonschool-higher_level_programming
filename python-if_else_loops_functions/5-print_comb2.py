678042eb95c598996a9d7a852f5da9d1acc0884c
Amaël Lounkokobi <8170@holbertonstudents.com>
2023-11-24 00:34:07 -0800

    fixing comment, delete 'prints' in comment

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# A program that prints numbers from 0 to 99
+# A program that show numbers from 0 to 99
 for num in range(99):
     print('{0:0>2d}, '.format(num), end='')
 else:

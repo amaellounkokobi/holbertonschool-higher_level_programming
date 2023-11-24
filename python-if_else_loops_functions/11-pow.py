4c094c968e8fba429fb43d3f9914b51eda0023e4
Amaël Lounkokobi <8170@holbertonstudents.com>
2023-11-24 05:51:36 -0800

    Exercices 11 - add minus and zero, fix the result

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -2,6 +2,16 @@
 
 # A function that computes a to the power of b and return the value
 def pow(a, b):
-    for index in range(b):
-        a *= a
-    return a
+    result = 1
+    
+    if b > 0:
+        for index in range(b):
+            result = result * a
+    elif b == 0:
+        pass
+    else:
+        for index in range(abs(b)):
+            result = result * a
+        result = 1 / result
+
+    return result

50882002fa27997cdb6e2d4e4e3ca45390a558b4
Amaël Lounkokobi <8170@holbertonstudents.com>
2023-11-24 03:27:57 -0800

    Eercices 7

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# Function that checks for lowercase character
+
+def islower(c):
+    if ord(c) > 96 and ord(c) < 123:
+        return True
+    else:
+        return False
+
+# Function outputs
+
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))

86f9fa7ffb5671328b3842649ef091231f9f9c9a
Amaël Lounkokobi <8170@holbertonstudents.com>
2023-11-23 23:45:23 -0800

    Exercices 5 - test with two print

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 # A program that prints numbers from 0 to 99
-for num in range(99):
-    print('{0:0>2d}, '.format(num), end='')
-
-print('{0:0>2d}, '.format(num + 1))
+for num in range(100):
+        if not num == 99:
+            print(num, end='')
+        else:
+            print('99')

be4e2592ec97d8e08dca97c7789fe0c01d5325ba
Amaël Lounkokobi <8170@holbertonstudents.com>
2023-11-24 05:15:27 -0800

    Exercices 9 - fix spaces

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,6 @@
 
 # Prints the last digit of a number
 def print_last_digit(number):
-    last_digit = abs(number) % 10 
+    last_digit = abs(number) % 10
     print(last_digit, end='')
     return last_digit
-

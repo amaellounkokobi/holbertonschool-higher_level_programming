716a497cf8e3faa1e91e81e128fa8b4061219982
Amaël Lounkokobi <8170@holbertonstudents.com>
2023-11-24 06:18:07 -0800

    Exercices 12 - fix pycodestyle

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,12 +2,12 @@
 
 # A function that prints the numbers from 1 to 100 separated by a space
 def fizzbuzz():
-    for n in range(1,101):
+    for n in range(1, 101):
         if n % 3 == 0 and n % 5 == 0:
             print('FizzBuzz ', end='')
-        elif  n % 3 == 0:
+        elif n % 3 == 0:
             print('Fizz ', end='')
-        elif  n % 5 == 0:
+        elif n % 5 == 0:
             print('Buzz ', end='')
         else:
             print('{} '.format(n), end='')

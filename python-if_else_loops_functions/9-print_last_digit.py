b69ecda56686bd1239d286af2016cca99d9ff9ec
Amaël Lounkokobi <8170@holbertonstudents.com>
2023-11-24 05:12:17 -0800

    Exercices 9

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# Prints the last digit of a number
+def print_last_digit(number):
+    last_digit = abs(number) % 10 
+    print(last_digit, end='')
+    return last_digit
+
+
+# Output
+
+print_last_digit(98)
+print_last_digit(0)
+r = print_last_digit(-1024)
+print(r)

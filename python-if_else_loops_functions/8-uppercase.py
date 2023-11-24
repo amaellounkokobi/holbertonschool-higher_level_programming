e1d8a5f7f8e413ee07120d733d4cb31f9085811b
Amaël Lounkokobi <8170@holbertonstudents.com>
2023-11-24 04:27:53 -0800

    Exercice 8 - fix space

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
 # character is a char
-
 def isLower(c):
     if ord(c) > 96 and ord(c) < 123:
         return True
     else:
         return False
 
-# String in uppercase
 
+# String in uppercase
 def uppercase(string):
     for c in string:
         print('{}'.format(c if not isLower(c) else chr(ord(c) - 32)), end='')

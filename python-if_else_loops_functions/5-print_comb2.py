dc4533e8017e619b8c837af7818d4abf1c972d9d
Amaël Lounkokobi <8170@holbertonstudents.com>
2023-11-24 01:24:05 -0800

    fix spaces in code

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-# A program that show numbers from 0 to 99
+# A program that shows numbers from 0 to 99
 for num in range(99):
     print('{0:0>2d}, '.format(num), end='')
 else:
     print('{0:0>2d}'.format(num + 1))
-
-

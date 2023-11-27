9cb2699d8aa9d3f930d0509a046bd013ee3172e1
Amaël Lounkokobi <8170@holbertonstudents.com>
2023-11-27 08:04:11 -0800

    An empty class Square that defines a square

---
+++ python-classes/0-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" Square module
+
+This module descibe a class that represents a square 
+
+Exemples:
+   Verifying if the classsquare exists:
+      
+      my_square = Square()
+      print(type(my_square))
+      print(my_square.__dict__)
+
+Attributes
+   No attributes.
+
+Classes
+   Square: Represent a square.
+
+ 
+
+"""
+
+
+class Square():
+    pass
+

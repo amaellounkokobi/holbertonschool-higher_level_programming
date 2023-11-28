842198a250bd082113630e3524d1b33018ce2f9a
Amaël Lounkokobi <8170@holbertonstudents.com>
2023-11-28 09:40:04 -0800

    Exercice 6 - fix program errors management

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -112,9 +112,9 @@ class Square():
 
         if not type(value) is tuple:
             raise TypeError('position must be a tuple of 2 positive integers')
-        elif (not type(value[0]) is int) and (not type(value[1] is int)):
+        elif (not type(value[0]) is int) or (not type(value[1] is int)):
             raise TypeError('position must be a tuple of 2 positive integers')
-        elif value[0] < 0 and value[1] < 0:
+        elif value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self.__position = value

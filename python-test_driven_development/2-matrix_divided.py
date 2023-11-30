f5f0841049657d6b4b373e23341dfeeb68d839f2
Amaël Lounkokobi <8170@holbertonstudents.com>
2023-11-30 15:09:33 -0800

    Fixing pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  This module contains a function that divides all elements of a matrix.
+""" This module contains a function that divides all elements of a matrix.
 
 Examples:
    >>> matrix = [
@@ -14,7 +14,7 @@ Examples:
 
 def matrix_divided(matrix, div=None):
     """ This function that divides all elements of a matrix
-    
+
     Args:
        matrix(list[lists[int/float]]): Matrix must be a list of lists of integers or floats.
        div(int/floar): The divided number must be an integer or a float. div can’t be equal to 0.
@@ -22,12 +22,14 @@ def matrix_divided(matrix, div=None):
     Returns:
        A new matrix of the same size,
        All elements of the matrix should be divided by div, rounded to 2 decimal places
-       
+
     Raise:
        TypeError exception with the message matrix must be a matrix (list of lists) of integers/floats
        ZeroDivisionError exception with the message division by zero
 
     """
+
+    """ Div exception """
     if div == 0:
         raise ZeroDivisionError('division by zero')
 
@@ -36,33 +38,35 @@ def matrix_divided(matrix, div=None):
 
     if not type(div) is int and not type(div) is float:
         raise TypeError('div must be a number')
-    
+
     if div == None:
         raise TypeError('division by zero')
-    
+
+    """ Matrix exception """
     line_len = len(matrix)
 
     if line_len == 0:
         raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
 
-    for l in range(1, line_len):
-        c_num = len(matrix[l])
-        c_num_prev = len(matrix[l - 1])
+    for line in range(1, line_len):
+        c_num = len(matrix[line])
+        c_num_prev = len(matrix[line - 1])
         if not c_num == c_num_prev:
             raise TypeError('Each row of the matrix must have the same size')
 
     column_len = len(matrix[0])
-    
-    for l in range(line_len):
-        for c in range(column_len):
-            if not type(matrix[l][c]) is int and not type(matrix[l][c]) is float:
+
+    for line in range(line_len):
+        for col in range(column_len):
+            if not type(matrix[line][col]) is int and not type(matrix[line][col]) is float:
                 raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
 
+    """ Function """
     new_matrix = []
-    
-    for l in range(line_len):
+
+    for line in range(line_len):
         new_matrix.append([])
-        for c in range(column_len):
-            new_matrix[l].append(round(matrix[l][c] / div, 2))
+        for col in range(column_len):
+            new_matrix[line].append(round(matrix[line][col] / div, 2))
 
     return new_matrix

6f545b807b9c52497829311d9a1d57a9ca7a097f
Amaël Lounkokobi <8170@holbertonstudents.com>
2023-11-30 14:34:38 -0800

    Exercices update and tests

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -28,4 +28,38 @@ def matrix_divided(matrix, div):
        ZeroDivisionError exception with the message division by zero
 
     """
-    pass
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+
+    if not type(div) is int and not type(div) is float:
+        raise TypeError('div must be a number')
+    
+    if div == None:
+        raise TypeError('division by zero')
+    
+    line_len = len(matrix)
+
+    if line_len == 0:
+        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+
+    for l in range(1, line_len):
+        c_num = len(matrix[l])
+        c_num_prev = len(matrix[l - 1])
+        if not c_num == c_num_prev:
+            raise TypeError('Each row of the matrix must have the same size')
+
+    column_len = len(matrix[0])
+    
+    for l in range(line_len):
+        for c in range(column_len):
+            if not type(matrix[l][c]) is int and not type(matrix[l][c]) is float:
+                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+
+    new_matrix = []
+    
+    for l in range(line_len):
+        new_matrix.append([])
+        for c in range(column_len):
+            new_matrix[l].append(round(matrix[l][c] / div, 2))
+
+    return new_matrix

4f1e77394ba906dd2686ee65e27b961f2198c83e
Amaël Lounkokobi <8170@holbertonstudents.com>
2023-12-02 07:33:51 -0800

    Exercices 4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Print square module
+
+This module descibe a function that prints a square.
+
+Exemples:
+   Print a square:
+
+>>> print_square(4)
+####
+####
+####
+####
+
+"""
+
+def print_square(size):
+    """ Printing a visual version of the square
+        parameter size
+
+    Args:
+           size (void): Size of the square.
+           the size must be a positive int value
+    Raise:
+           TypeError, Size must be an integer
+           TypeError, Size must be >= 0
+
+    """
+
+    """ Exception handling """
+
+    if not type(size) is int:
+        raise TypeError('size must be an integer')
+
+    if size < 0:
+        raise ValueError('size must be >= 0')
+
+    """ Function """
+    for line in range(size):
+        for column in range(size):
+            print('#', end='')
+        print('')
